41086d32de599f3c792dfa76a28d890b8544426f
Tejas-thawari-05 <tejasthawri@gmail.com>
2024-12-15 13:02:47 +0530

    new project only admin module is done

---
+++ E-Commerse_Site/src/main/java/com/ecom/ECommerseSiteApplication.java
@@ -0,0 +1,13 @@
+package com.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerseSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerseSiteApplication.class, args);
+	}
+
+}
